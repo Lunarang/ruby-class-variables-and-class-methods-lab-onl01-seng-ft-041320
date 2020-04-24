683e2cd89cca67c778e6e2010a426bea8d052c5a
Erin Mettille <ecmettille@gmail.com>
2020-04-24 19:29:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,15 @@ class Song
   end
   
   def self.artist_count
-    artist_count = {}
+    @@artist_count = {}
+    @@artist_count.each do |artist|
+      if @@artist_count.include? artist 
+        @@artist_count[artist] += 1 
+      else
+        @@artist_count[artist] = 1 
+      end
+    end
+    @@artist_count
   end
 
 end
