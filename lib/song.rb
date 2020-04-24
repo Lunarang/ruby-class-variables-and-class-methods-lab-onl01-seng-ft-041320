4f21360aa8a17bd8c6967c586b89763080191b0d
Erin Mettille <ecmettille@gmail.com>
2020-04-24 18:53:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,25 @@
 class Song 
   attr_accessor :name, :artist, :genre
-  @@song_count = 0 
-  
+ 
+  @@count = 0 
+  @@genres = []
+  @@artists = []
   def self.count 
     @@song_count
   end 
 
+  def self.genres 
+    @@genres 
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
   def initialize(name, artist, genre)
-    
+    @@count += 1 
+    @@genres << genre 
+    @@artists << artist
   end
 
 
